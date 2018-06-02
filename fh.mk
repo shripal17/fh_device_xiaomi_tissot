26022c6779b8e73c509564d775cb5fb16f388197
Shripal Jain <shripal17@gmail.com>
2018-06-02 08:15:29 +0000

    tissot: Fire Up!

--- fh.mk
+++ fh.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common FireHound stuff.
+$(call inherit-product, vendor/fh/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := fh_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
